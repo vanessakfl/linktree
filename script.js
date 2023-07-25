3ed83097f09b9782e6cdcdacd6ac69b7f62cec5b
Vanessa Ferreira <vanessakferreiralopes@gmail.com>
2023-07-25 12:50:58 -0300

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,30 @@
+function toggleMode() {
+  const html = document.documentElement
+
+  html.classList.toggle("light")
+  // if(html.classList.contains('light')) {
+  //    html.classList.remove('light')
+  // } else {
+  //  html.classList.add('light')
+  // }
+
+  //pegar a tag img
+  const img = document.querySelector("#profile img")
+
+  //substituir a imagem
+  if (html.classList.contains("light")) {
+    //se tiver light mode, adicionar a imagem light
+    img.setAttribute("src", "./assets/avatar-light.png")
+    img.setAttribute(
+      "alt",
+      "Foto de Mayk Brito sorrindo, usando óculos e camisa preta, barba e fundo amarelo"
+    )
+  } else {
+    //se tiver sem light mode, manter a imagem normal
+    img.setAttribute("src", "./assets/avatar.png")
+    img.setAttribute(
+      "alt",
+      "Foto de Mayk Brito sorrindo, usando óculos de sol, camisa preta, barba e fundo azul"
+    )
+  }
+}
